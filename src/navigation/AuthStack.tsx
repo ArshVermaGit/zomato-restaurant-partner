fe0f34c72f974b7fe707b42054ea824092aafd30
Arsh Verma <arshverma.dev@gmail.com>
2025-12-14 21:46:51 +0530

    Create AuthStack.tsx

---
+++ src/navigation/AuthStack.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/auth/LoginScreen';
+
+const Stack = createStackNavigator();
+
+export const AuthStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            {/* OTP Screen can be added here if needed */}
+        </Stack.Navigator>
+    );
+};
