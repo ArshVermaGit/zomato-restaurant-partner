f3ade8bc845297a1c7d8f881dcebb5f60509e9e1
Arsh Verma <arshverma.dev@gmail.com>
2025-12-14 21:41:40 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jest.setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-?native|@react-native|react-native|@react-navigation|@reduxjs/toolkit|react-redux|axios|immer)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
