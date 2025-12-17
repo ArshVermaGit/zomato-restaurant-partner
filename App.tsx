32671cc63a897a26882dcda37a16e37eef7468f4
Arsh Verma <arshverma.dev@gmail.com>
2025-12-18 00:23:17 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,10 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store, persistor } from './src/store';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
+import { useNotifications } from './src/hooks/useNotifications';
+
 const App = () => {
+  useNotifications();
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
