a466ebfb2d662fb6fd0fffe0a67df28ba33f0d2a
Arsh Verma <arshverma.dev@gmail.com>
2025-12-17 14:00:43 +0530

    Update OrdersStack.tsx

--- src/navigation/OrdersStack.tsx
+++ src/navigation/OrdersStack.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import OrdersScreen from '../screens/orders/OrdersScreen';
 import KitchenDisplayScreen from '../screens/orders/KitchenDisplayScreen';
-// import OrderDetailScreen from '../screens/orders/OrderDetailScreen'; // Will implement next if needed
+import { RestaurantOrderDetailScreen } from '../screens/orders/RestaurantOrderDetailScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +11,7 @@ export const OrdersStack = () => {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="OrdersList" component={OrdersScreen} />
             <Stack.Screen name="KitchenDisplay" component={KitchenDisplayScreen} />
-            {/* <Stack.Screen name="OrderDetail" component={OrderDetailScreen} /> */}
+            <Stack.Screen name="RestaurantOrderDetail" component={RestaurantOrderDetailScreen} />
         </Stack.Navigator>
     );
 };
